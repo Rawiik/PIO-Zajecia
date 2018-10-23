97bfda00d0634290e6c1ad52d300955c255fd1f0
Rawiik <33567377+Rawiik@users.noreply.github.com>
2018-10-23 16:40:27 +0200

    Update PierwszaAplikacja.java

--- src/pierwszaaplikacja/PierwszaAplikacja.java
+++ src/pierwszaaplikacja/PierwszaAplikacja.java
@@ -1,26 +1,41 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package pierwszaaplikacja;
 
 import java.util.Random;
+import java.util.Scanner;
 
-public class PierwszaAplikacja {
+public class DiceCubeGame {
 
     public static void main(String[] args) {
 
+        
+        
+        
+        
         Random dice = new Random();
-        Gracz ja = new Gracz();
-        ja.setName(null);
+        Scanner read = new Scanner(System.in);
+        Player ja = new PlayerComp();
+        int wybor;
+        
+        System.out.println("---- WITAMY W DICECUBEGAME ----");
+        System.out.println("    1)GRAJ PRZECIWKO KOMPUTEROWI\n    2)GRAJ PRZECIWKO GRACZOWI");
+//        System.out.print("Wybierz numer: "); wybor = read.nextInt();
+//        if (wybor == 1) {
+//            ja = new PlayerComp();
+//        } else if(wybor == 2) {
+//            ja = new PlayerHuman("Ferdynand Kiepski");
+//            
+//        }
+        
+        
+        
+        
 
         int number = 0, quess = 0;
 
         do {
 
             number = dice.nextInt(6) + 1;
-            System.out.println("Wylosowany (number)" + number);
+            System.out.println("Wylosowany typ: " + number);
             quess = ja.quessNumber();
             System.out.println("Strzał " + ja.getName() + " " + quess);
             if (quess == number) {
